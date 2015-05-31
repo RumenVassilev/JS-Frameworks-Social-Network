feab562f72e0ef50030c1e556a204a6beb1ca405
RumenVassilev <roumen.vasilev@gmail.com>
2015-05-31 17:38:11 +0300

    removing files

--- js/controllers/RegisterController.js
+++ js/controllers/RegisterController.js
@@ -1,13 +1,12 @@
 'use strict';
 
 app.controller('RegisterController',
-    function ($scope, $rootScope, $location, townsService, authService, notifyService) {
+    function ($scope, $rootScope, $location, authService, notifyService) {
         $rootScope.showRightSidebar = false;
         $rootScope.ngViewSize = 'col-md-10';
         $rootScope.pageTitle = "Register";
 
         $scope.userData = {townId: null};
-        $scope.towns = townsService.getTowns();
 
         $scope.register = function(userData) {
             authService.register(userData,
